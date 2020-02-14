28b4a61733ff31bd0a6f9ac4a1a17727271040c2
furic <zhjcnnet@gmail.com>
2020-02-14 15:18:17 +0800

    veriify stypescript

--- scripts/utils/verifyTypeScriptSetup.js
+++ scripts/utils/verifyTypeScriptSetup.js
@@ -249,11 +249,11 @@ function verifyTypeScriptSetup() {
     writeJson(paths.appTsConfig, appTsConfig);
   }
 
-  // Reference `react-scripts` types
+  // Reference `cjet` types
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="cjet" />${os.EOL}`
     );
   }
 }
