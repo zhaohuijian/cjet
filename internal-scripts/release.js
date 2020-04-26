87c1d33521aa0c9245920378a5edaeab0a7066aa
gaoletian <67890211@qq.com>
2020-04-26 15:09:47 +0800

    chore: update release script

--- internal-scripts/release.js
+++ internal-scripts/release.js
@@ -3,34 +3,30 @@ const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
 const currentVersion = require('../package.json').version;
-const { prompt } = require('enquirer');
+const {prompt} = require('enquirer');
 const execa = require('execa');
 
 const preId = args.preid || (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0]) || 'alpha';
 const isDryRun = args.dry;
 const skipTests = args.skipTests;
 const skipBuild = args.skipBuild;
-const packages = fs
-  .readdirSync(path.resolve(__dirname, '../packages'))
-  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'));
 
 const versionIncrements = ['patch', 'minor', 'major', 'prepatch', 'preminor', 'premajor', 'prerelease'];
 
 const inc = i => semver.inc(currentVersion, i, preId);
 const bin = name => path.resolve(__dirname, '../node_modules/.bin/' + name);
-const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts });
-const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg);
+const run = (bin, args, opts = {}) => execa(bin, args, {stdio: 'inherit', ...opts});
 
 async function main() {
   let targetVersion = args._[0];
 
   if (!targetVersion) {
     // no explicit version, offer suggestions
-    const { release } = await prompt({
+    const {release} = await prompt({
       type: 'select',
       name: 'release',
       message: 'Select release type',
-      choices: versionIncrements.map(i => `${i} (${inc(i)})`).concat(['custom'])
+      choices: versionIncrements.map(i => `${i} (${inc(i)})`).concat(['custom']),
     });
 
     if (release === 'custom') {
@@ -39,7 +35,7 @@ async function main() {
           type: 'input',
           name: 'version',
           message: 'Input custom version',
-          initial: currentVersion
+          initial: currentVersion,
         })
       ).version;
     } else {
@@ -51,10 +47,10 @@ async function main() {
     throw new Error(`invalid target version: ${targetVersion}`);
   }
 
-  const { yes } = await prompt({
+  const {yes} = await prompt({
     type: 'confirm',
     name: 'yes',
-    message: `Releasing v${targetVersion}. Confirm?`
+    message: `Releasing v${targetVersion}. Confirm?`,
   });
 
   if (!yes) {
@@ -69,11 +65,6 @@ async function main() {
 
   // update all package versions and inter-dependencies
   updateVersions(targetVersion);
-
-  // build all packages with types
-  if (!skipBuild) {
-    await run('yarn', ['build']);
-  }
 
   // all good...
   if (isDryRun) {
@@ -93,9 +84,7 @@ async function main() {
     // publish packages
     const releaseTag = Array.isArray(semver.prerelease(targetVersion)) ? semver.prerelease(targetVersion)[0] : 'latest';
 
-    for (const pkg of packages) {
-      await publish(pkg, releaseTag);
-    }
+    await publish(releaseTag);
 
     // push to GitHub
     await run('git', ['tag', `v${targetVersion}`]);
@@ -108,21 +97,12 @@ function updateVersions(version) {
   console.log('Updating versions...');
   // 1. update root package.json
   updatePackage(path.resolve(__dirname, '..'), version);
-  // 2. update all packages
-  packages.forEach(p => updatePackage(getPkgRoot(p), version));
 }
 
 function updatePackage(pkgRoot, version) {
   const pkgPath = path.resolve(pkgRoot, 'package.json');
   const pkg = readPkg(pkgRoot);
   pkg.version = version;
-  if (pkg.dependencies) {
-    Object.keys(pkg.dependencies).forEach(dep => {
-      if (packages.includes(dep)) {
-        pkg.dependencies[dep] = version;
-      }
-    });
-  }
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
 }
 
@@ -131,12 +111,12 @@ function readPkg(pkgRoot) {
   return JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
 }
 
-async function publish(pkgName, releaseTag) {
-  const pkgRoot = getPkgRoot(pkgName);
+async function publish(releaseTag) {
+  const pkgRoot = path.resolve(__dirname, '..');
   const pkg = readPkg(pkgRoot);
   if (!pkg.private) {
     await run('yarn', ['publish', '--non-interactive', '--tag', releaseTag], {
-      cwd: pkgRoot
+      cwd: pkgRoot,
     });
   }
 }
